45ea455822019dc25d3ece404c8d7ba7eeda5167
Mihai <mihaiftr@users.noreply.github.com>
2014-11-24 21:50:58 +0200

    guests: generate code (unique within an event)

--- guests.go
+++ guests.go
@@ -5,16 +5,57 @@ import (
 	"appengine/datastore"
 	"appengine/user"
 	"errors"
+	"fmt"
+	"math/rand"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type guestT struct {
 	Name, Email, HostEmail string
+	// auto-generated, uniquely identifies a guest in an event
+	Code string
 }
+
+const (
+	guestCodeLen   = 5
+	guestCodeSpace = "0123456789" +
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz"
+	guestCodeAttempts = 10
+)
 
 func init() {
 	http.Handle(addGuestURL, appHandler(addGuestHandler))
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+// Generate a new guest code different from all codes in ‘exclude’.
+// If a unique code isn't generated after several attempts, give up and
+// return an error.
+func generateGuestCode(exclude []guestT) (code string, err error) {
+	set := make(map[string]interface{})
+	for _, guest := range exclude {
+		set[guest.Code] = nil
+	}
+
+	for n := 0; n < guestCodeAttempts; n++ {
+		code = ""
+		for len(code) < guestCodeLen {
+			i := rand.Intn(len(guestCodeSpace))
+			code += guestCodeSpace[i : i+1]
+		}
+		_, ok := set[code]
+		if !ok {
+			return
+		}
+	}
+	return "", fmt.Errorf("Failed to generate unique guest code "+
+		"after %v attempts", guestCodeAttempts)
 }
 
 func addGuestHandler(w http.ResponseWriter, r *http.Request) *appError {
@@ -62,20 +103,26 @@ func addGuestPOSTHandler(w http.ResponseWriter, r *http.Request) *appError {
 			return err
 		}
 
+		code, err := generateGuestCode(event.Guests)
+		if err != nil {
+			return err
+		}
+
 		event.Guests = append(event.Guests, guestT{
 			Name:      name,
 			Email:     email,
 			HostEmail: hostEmail,
+			Code:      code,
 		})
 
-		_, err := datastore.Put(c, key, event)
+		_, err = datastore.Put(c, key, event)
 		return err
 	}, nil)
 	if err == datastore.ErrNoSuchEntity {
 		return &appError{err, "Event not found", http.StatusNotFound}
 	}
 	if err != nil {
-		return &appError{err, "Error getting event",
+		return &appError{err, "Error adding guest",
 			http.StatusInternalServerError}
 	}
 
