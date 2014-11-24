0544248c723bdc1f02b944fef4e355c17edc967d
Mihai <mihaiftr@users.noreply.github.com>
2014-11-24 16:55:33 +0200

    urls: return StatusNotFound (404) for unknown URLs

--- homepage.go
+++ homepage.go
@@ -1,13 +1,19 @@
 package mypkg
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func init() {
-	http.Handle("/", appHandler(homepageHandler))
+	http.Handle("/", appHandler(rootHandler))
 }
 
-func homepageHandler(w http.ResponseWriter, r *http.Request) *appError {
+func rootHandler(w http.ResponseWriter, r *http.Request) *appError {
+	if r.RequestURI != "/" {
+		return &appError{
+			fmt.Errorf("URL not found: ‘%v’", r.RequestURI),
+			"Not Found", http.StatusNotFound}
+	}
 	return execNavTempl(r, w, "homepage.html", map[string]interface{}{})
 }
